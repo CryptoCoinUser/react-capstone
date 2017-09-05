ea0ff1dc735746012a1b33cec22f1421c2619680
CryptoCoinUser <adkantor@gmail.com>
2017-09-05 14:18:46 -0700

    added 30 second loop demo GIF

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -5,6 +5,8 @@ export default function LoginPage() {
     	<div id="loginWrapper">
 	    	<h2>Watch My Bitcoin Address App</h2>
 	    	<a href={'/api/auth/google'}>Login with Google</a>
+	    	<h4>30-second looping demo:</h4>
+	    	<img src="/demoLoop.gif" alt="30-second animated demo" />
     	</div>
     	)
 }
