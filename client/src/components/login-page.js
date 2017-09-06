6016ea71348ac99c2e32f5cbe39d0157bc07caa1
CryptoCoinUser <adkantor@gmail.com>
2017-09-06 13:28:06 -0700

    replaced demo gif with mp4, login page tweaks

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -4,9 +4,12 @@ export default function LoginPage() {
     return (
     	<div id="loginWrapper">
 	    	<h2>Watch My Bitcoin Address App</h2>
-	    	<a href={'/api/auth/google'}>Login with Google</a>
-	    	<h4>30-second looping demo:</h4>
-	    	<img src="/demoLoop.gif" alt="30-second animated demo" />
+	    	<a href={'/api/auth/google'} id="loginLink">Login with Google</a>
+	    	<h4>30-second demo video:</h4>
+	    	<video controls autoplay preload="auto" width='640' height='821' poster='/videoPoster.png'>
+				<source src='/demo30sec.mp4' type='video/mp4' />
+				Your browser does not support the HTML5 'video' tag
+			</video>
     	</div>
     	)
 }
